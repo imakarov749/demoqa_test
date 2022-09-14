cf49315fcaf241bd091c8b351f5ceb2f0a14adba
imakarov749 <ivan.makarov199716@gmail.com>
2022-09-14 10:46:08 +0300

    Изменил локатор для выбора даты в календаре, внес общие правки в тест

--- test_automation-practice-form.py
+++ test_automation-practice-form.py
@@ -11,9 +11,9 @@ def test_fill_form():
     l_name_for_type = 'Ivanov'
     email_for_type = 'ivan.ivanov@kljh.com'
     number_for_type = '7123456789'
-    birth_for_type = '12 Dec 1912'
+    # birth_for_type = '12 Dec 1912' - больше не нужна, но я оставлю для второго варианта ввода даты рождения
     adress_for_type = 'Moscow'
-    # file_path = os.path.abspath("files_for_test/photo_image.jpg")
+    file_path = os.path.abspath("files_for_test/photo_image.jpg")
 
     # вводим имя
     first_name = browser.element('#firstName')
@@ -35,14 +35,13 @@ def test_fill_form():
     number = browser.element('#userNumber')
     number.should(be.blank).type(number_for_type)
 
-    # вводим дату рождения
-    # не уверен, что эти селекторы хорошие, но иначе не догадался, как сделать
-    birth_calendar = browser.element('#dateOfBirthInput').click()
-    month_select = browser.element('.react-datepicker__month-select').click().element('[value="11"]').click()
-    year_select = browser.element('.react-datepicker__year-select').click().element('[value="1912"]').click()
-    day_select = browser.element('[aria-label="Choose Thursday, December 12th, 1912"]').click()
+    # вводим дату рождения через выбор в календаре
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click().element('[value="11"]').click()
+    browser.element('.react-datepicker__year-select').click().element('[value="1912"]').click()
+    browser.element('.react-datepicker__day--012').click()
 
-    # тут мб зря использовал Keys, но через clear не получилось
+    # v2.можно ввести дату рождения вручную
     # birth.clear().set_value('12 Dec 1912').press_enter()
     # birth.send_keys(Keys.CONTROL + 'a').type(birth_for_type).press_enter()
 
@@ -74,24 +73,26 @@ def test_fill_form():
     submit = browser.element('#submit')
     submit.click()
 
+    # проверяем, что окно действительно открылось
     submitting_window = browser.element('#example-modal-sizes-title-lg')
     submitting_window.should(be.visible).should(have.text('Thanks for submitting the form'))
 
-    def test_check_form():
-        check_fields = browser.element('.table-responsive')
-        check_fields.should(have.text(f_name_for_type))
-        check_fields.should(have.text(l_name_for_type))
-        check_fields.should(have.text(email_for_type))
-        check_fields.should(have.text('Male'))
-        check_fields.should(have.text(number_for_type))
+    # проверки полей результирующей таблицы
+    check_fields = browser.element('.table-responsive')
+    check_fields.should(have.text(f_name_for_type))
+    check_fields.should(have.text(l_name_for_type))
+    check_fields.should(have.text(email_for_type))
+    check_fields.should(have.text('Male'))
+    check_fields.should(have.text(number_for_type))
 
-        # проверяем дату
-        # все равно) как текст - это просто указать текст, который мы ожидаем, в проверке но!
-        # кажется, что правильнее было бы запоминать то, что ввели в начале, а потом искать по частичному совпадению
-        check_fields.re.findall(birth_for_type)
-        check_fields.should(have.text('12December,1912'))
+    # проверяем дату
+    # все равно) как текст - это просто указать текст, который мы ожидаем, в проверке но!
+    # кажется, что правильнее было бы запоминать то, что ввели в начале, а потом искать по частичному совпадению
+    check_fields.should(have.text('12 December,1912'))
 
-        check_fields.should(have.text('photo_image.jpg'))
+    # проверяем загруженный файл
+    check_fields.should(have.text('photo_image.jpg'))
 
+    # закрываем результирующую таблицу
     close_button = browser.element('#closeLargeModal')
     close_button.click()
