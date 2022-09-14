915a49132120948f2435efc7af1a054c866ceec9
imakarov749 <ivan.makarov199716@gmail.com>
2022-09-14 10:47:38 +0300

    Добавил закрытие браузера после теста в фикстуру

--- 1.py
+++
@@ -1,4 +0,0 @@
-import pathlib
-
-file_path = pathlib.Path(r'C:\Users\ancor_makarov\PycharmProjects\demoqa_test\files_for_test\photo_image.jpg')
-print(file_path)
--- conftest.py
+++ conftest.py
@@ -2,7 +2,8 @@ from selene.support.shared import browser
 import pytest
 from selene import have, be
 
-#открываем браузер, проверяем корректность url закрываем после теста
+
+# открываем браузер, проверяем корректность url, закрываем после теста
 @pytest.fixture()
 def demo_qa_open_browser():
     browser.config.hold_browser_open = True
@@ -13,10 +14,10 @@ def demo_qa_open_browser():
     browser.should(have.url('https://demoqa.com/automation-practice-form'))
     browser.element('.main-header').should(be.visible)
     yield
-    #browser.close()
+    browser.close()
 
 
-#задаем конфигурацию браузера
+# задаем конфигурацию браузера
 @pytest.fixture(autouse=True)
 def browser_configure(demo_qa_open_browser):
     browser.driver.set_window_size(1920, 1080)
